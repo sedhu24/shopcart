5359cd17dbdd0ceed77339c88c2b9df650b471db
sedhu24 <smilesedhu001@gmail.com>
2023-04-11 17:28:47 +0530

    filepicker permissions

--- lib/constants/utils.dart
+++ lib/constants/utils.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
 void showSnackBar(BuildContext context, String text) {
@@ -10,10 +11,22 @@ void showSnackBar(BuildContext context, String text) {
   );
 }
 
-pickimages() async {
+Future<List<File>> pickimages() async {
   List<File> images = [];
 
-  try {} catch (e) {
+  try {
+    var file = await FilePicker.platform.pickFiles(
+      allowMultiple: true,
+    );
+    if (file != null && file.files.isNotEmpty) {
+      for (int i = 0; i < file.files.length; i++) {
+        images.add(File(file.files[i].path!));
+        debugPrint(images.toString());
+      }
+    }
+  } catch (e) {
     debugPrint(e.toString());
   }
+
+  return images;
 }
--- lib/features/admin/views/add_products_screen.dart
+++ lib/features/admin/views/add_products_screen.dart
@@ -1,5 +1,8 @@
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:shopcart/constants/utils.dart';
 import 'package:shopcart/common/custom_buttom.dart';
 import 'package:shopcart/common/custom_textfiel.dart';
 import 'package:shopcart/constants/global_variables.dart';
@@ -30,6 +33,8 @@ class _AddProductScreenState extends State<AddProductScreen> {
 
   String categories = 'Mobiles';
 
+  List<File> images = [];
+
   List<String> productCategories = [
     'Mobiles',
     'Essentials',
@@ -37,6 +42,14 @@ class _AddProductScreenState extends State<AddProductScreen> {
     'Books',
     'Fashion',
   ];
+
+  void selectimages() async {
+    var result = await pickimages();
+
+    setState(() {
+      images = result;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -71,35 +84,61 @@ class _AddProductScreenState extends State<AddProductScreen> {
                 const SizedBox(
                   height: 20,
                 ),
-                DottedBorder(
-                  color: GlobalVariables.secondaryColor,
-                  borderType: BorderType.RRect,
-                  radius: const Radius.circular(10),
-                  dashPattern: const [10, 4],
-                  strokeCap: StrokeCap.round,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    width: double.infinity,
-                    height: 150,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Icon(Icons.folder_open_outlined, size: 40),
-                        const SizedBox(
-                          height: 15,
+                images.isNotEmpty
+                    ? GestureDetector(
+                        onTap: selectimages,
+                        child: CarouselSlider(
+                          items: images.map((i) {
+                            return Builder(builder: (BuildContext context) {
+                              return Image.file(
+                                i,
+                                fit: BoxFit.cover,
+                                height: 200,
+                              );
+                            });
+                          }).toList(),
+                          options: CarouselOptions(
+                            autoPlay: true,
+                            pauseAutoPlayInFiniteScroll: true,
+                            viewportFraction: 1,
+                            height: 200,
+                            enlargeCenterPage: true,
+                          ),
                         ),
-                        Text(
-                          'Select Product Images',
-                          style: TextStyle(color: Colors.grey.shade400),
+                      )
+                    : GestureDetector(
+                        onTap: selectimages,
+                        child: DottedBorder(
+                          color: GlobalVariables.secondaryColor,
+                          borderType: BorderType.RRect,
+                          radius: const Radius.circular(10),
+                          dashPattern: const [10, 4],
+                          strokeCap: StrokeCap.round,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            width: double.infinity,
+                            height: 150,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Icon(Icons.folder_open_outlined,
+                                    size: 40),
+                                const SizedBox(
+                                  height: 15,
+                                ),
+                                Text(
+                                  'Select Product Images',
+                                  style: TextStyle(color: Colors.grey.shade400),
+                                ),
+                              ],
+                            ),
+                          ),
                         ),
-                      ],
-                    ),
-                  ),
-                ),
+                      ),
                 const SizedBox(
-                  height: 1,
+                  height: 10,
                 ),
                 CustomTextfield(
                   hint: "Product Name",
