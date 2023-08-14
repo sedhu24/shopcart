319cb6f1add2990eda35b2dee3ebbe87717feff1
sedhu24 <smilesedhu001@gmail.com>
2023-08-14 11:46:33 +0530

    gpay configuration started

---
+++ lib/features/address/screen/address_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:shopcart/constants/global_variables.dart';
+
+class AddressScreen extends StatefulWidget {
+  static const String routeName = '/address-screen';
+  const AddressScreen({super.key});
+
+  @override
+  State<AddressScreen> createState() => _AddressScreenState();
+}
+
+class _AddressScreenState extends State<AddressScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(50),
+        child: AppBar(
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+              gradient: GlobalVariables.appBarGradient,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
